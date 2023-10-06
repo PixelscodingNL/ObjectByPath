266e17297ff82dda84ec53465903d302472b5b3b
Julian Kaagman <jkaagman@letsbuildit.nl>
2023-10-06 11:22:27 +0200

    Fix test

--- TestProject/UnitTestSettingValues.cs
+++ TestProject/UnitTestSettingValues.cs
@@ -47,22 +47,22 @@ public class UnitTestSettingValues
 	{
 		Dictionary<string, object> dictionary = new Dictionary<string, object>
 		{
-			{ "subpolis[0].mr[0].bestuurder[1].geboortedatum", "Value2" },
-			{ "subpolis[1].mr[1].bestuurder[3].geboortedatum", "Value16" },
-			{ "subpolis[0].mr[1].bestuurder[3].geboortedatum", "Value8" },
-			{ "subpolis[0].mr[1].bestuurder[0].geboortedatum", "Value5" },
-			{ "subpolis[1].mr[0].bestuurder[3].geboortedatum", "Value12" },
-			{ "subpolis[0].mr[0].bestuurder[2].geboortedatum", "Value3" },
-			{ "subpolis[1].mr[1].bestuurder[0].geboortedatum", "Value13" },
-			{ "subpolis[0].mr[1].bestuurder[1].geboortedatum", "Value6" },
-			{ "subpolis[1].mr[0].bestuurder[0].geboortedatum", "Value9" },
-			{ "subpolis[1].mr[1].bestuurder[2].geboortedatum", "Value15" },
-			{ "subpolis[0].mr[0].bestuurder[0].geboortedatum", "Value1" },
-			{ "subpolis[0].mr[0].bestuurder[3].geboortedatum", "Value4" },
-			{ "subpolis[1].mr[1].bestuurder[1].geboortedatum", "Value14" },
-			{ "subpolis[1].mr[0].bestuurder[1].geboortedatum", "Value10" },
-			{ "subpolis[0].mr[1].bestuurder[2].geboortedatum", "Value7" },
-			{ "subpolis[1].mr[0].bestuurder[2].geboortedatum", "Value11" },
+			{ "subpolis[0].mr[0].bestuurder[1].geboortedatum", 2 },
+			{ "subpolis[1].mr[1].bestuurder[3].geboortedatum", 16 },
+			{ "subpolis[0].mr[1].bestuurder[3].geboortedatum", 8 },
+			{ "subpolis[0].mr[1].bestuurder[0].geboortedatum", 5 },
+			{ "subpolis[1].mr[0].bestuurder[3].geboortedatum", 12 },
+			{ "subpolis[0].mr[0].bestuurder[2].geboortedatum", 3 },
+			{ "subpolis[1].mr[1].bestuurder[0].geboortedatum", 13 },
+			{ "subpolis[0].mr[1].bestuurder[1].geboortedatum", 6 },
+			{ "subpolis[1].mr[0].bestuurder[0].geboortedatum", 9 },
+			{ "subpolis[1].mr[1].bestuurder[2].geboortedatum", 15 },
+			{ "subpolis[0].mr[0].bestuurder[0].geboortedatum", 1 },
+			{ "subpolis[0].mr[0].bestuurder[3].geboortedatum", 4 },
+			{ "subpolis[1].mr[1].bestuurder[1].geboortedatum", 14 },
+			{ "subpolis[1].mr[0].bestuurder[1].geboortedatum", 10 },
+			{ "subpolis[0].mr[1].bestuurder[2].geboortedatum", 7 },
+			{ "subpolis[1].mr[0].bestuurder[2].geboortedatum", 11 }
 		};
 
 		var dict = dictionary.SortDictionaryDescendingKeynames();
@@ -73,8 +73,8 @@ public class UnitTestSettingValues
 		var counter = total;
 		foreach (var kvp in dict)
 		{
-			_testOutputHelper.WriteLine($"{kvp.Key}: {kvp.Value}, checking if Value ends with {counter}.");
-			Assert.True(kvp.Value.ToString()!.EndsWith(counter.ToString()));
+			_testOutputHelper.WriteLine($"{kvp.Key}: {kvp.Value}, checking to be value {counter}.");
+			Assert.True((int)kvp.Value == counter);
 			counter--;
 		}
 	}
