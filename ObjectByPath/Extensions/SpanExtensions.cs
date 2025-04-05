336e3e26df51cd965d48d082921b021728d5dd17
Julian <jpa.kaagman@gmail.com>
2025-04-05 21:08:58 +0200

    formatting in extensions file

--- ObjectByPath/Extensions/SpanExtensions.cs
+++ ObjectByPath/Extensions/SpanExtensions.cs
@@ -1,15 +1,12 @@
 namespace Pixelscoding.ObjectByPath.Extensions;
 
-using System;
-
 public static class SpanExtensions
 {
-	public static int CommonPrefixLength<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> other)
-		where T : IEquatable<T>
+	public static int CommonPrefixLength<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> other) where T : IEquatable<T>
 	{
-		int minLength = Math.Min(span.Length, other.Length);
+		var minLength = Math.Min(span.Length, other.Length);
 
-		for (int i = 0; i < minLength; i++)
+		for (var i = 0; i < minLength; i++)
 		{
 			if (!span[i].Equals(other[i]))
 			{
